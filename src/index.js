94a8ce864c31cd09c50cb30b5c8e81380834340f
sandrahfiestas <shfiestas@gmail.com>
2020-02-18 11:28:10 -0500

    actualizado al 180220

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import validator from './validator.js';
 
+rValida.style.display = 'none';
+rInvalida.style.display = 'none';
+
+document.getElementById("bValidar").addEventListener('click', () => {
+let valorIngresado = document.getElementById("creditCardNumber").value;
+let creditCardNumber = validator.isValid(valorIngresado);
+
+let valor = document.getElementById("creditCardNumber").value;
+let validMask = validator.maskify(valor);
+
+
+
+});
+
 console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,63 @@
 const validator = {
-  // ...
+
+  isValid (creditCardNumber){
+
+// INVIERTE NUMERO
+const newArr = [];
+for(let i= creditCardNumber.length-1; i >=0; i--){
+  // console.log( typeof cardNumber[i] );
+  // console.log( parseInt( cardNumber[i] ) );
+  newArr.push( parseInt( creditCardNumber[i] ) );
+}
+  console.log(newArr);
+
+//MULTIPLICAR lOS PARES
+let multiPar = [];
+for(let i=0; i<newArr.length; ++i){
+ if (i%2 !==0) {
+   multiPar.push(newArr[i]*2);
+   }
+ else{
+   multiPar.push(newArr[i]*1);
+   }
+}
+console.log('Multi par: ' + multiPar);
+
+
+// SUMADO DOS DIGITOS
+   let sumPar = [];
+    for(let i=0; i<multiPar.length; ++i){
+      if(parseInt(multiPar[i])>=10){
+        sumPar.push(multiPar[i] - 9);
+      }
+        else {
+          sumPar.push(multiPar[i]*1);
+        }
+        rValida.style.display = 'block';
+      }
+      console.log(sumPar);
+      let sumArray = sumPar.reduce(function(a,b){
+        return a+b;});
+        console.log('Suma Total:  '+sumArray);
+
+
+        if(sumArray%10 ==0){
+          alert('numero valido');
+        }
+        else{
+          alert('numero invalido');
+        }
+}
+  ,
+  maskify (validMask){
+    let paraMascara = validMask;
+    let enmascarado = paraMascara.replace(/.(?=.{4,}$)/g, '#');
+    console.log(paraMascara);
+    console.log(enmascarado);
+
+  }
 };
 
 export default validator;
+
+//tc valida 4083952015263
